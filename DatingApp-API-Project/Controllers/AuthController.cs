d04a0f2e94bae47445b7b53722aa0429a10eeefe
Rafiq Mattoo <Rafiq.Mattoo@gboiq.com>
2020-09-21 03:20:03 +0530

    Add project files.

---
+++ DatingApp-API-Project/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("CorsPolicy")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository authrepository;
+        public IConfiguration _config { get; set; }
+        public AuthController(IAuthRepository authRepository, IConfiguration config)
+        {
+            this._config = config;
+            this.authrepository = authRepository;
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserDataObject userDataObject)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            userDataObject.UserName = userDataObject.UserName.ToLower();
+            if (await authrepository.UserExists(userDataObject.UserName))
+            {
+                return BadRequest("User Already Exists in the System");
+            }
+
+            var userCreated = new User
+            {
+                Username = userDataObject.UserName
+            };
+
+            var createdUser = await authrepository.Register(userCreated, userDataObject.Password);
+            return StatusCode(201);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(UserDataObject userDataObject)
+        {
+            var userDatatobject = await authrepository.Login(userDataObject.UserName, userDataObject.Password);
+            if (userDatatobject == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]{
+                  new Claim(ClaimTypes.NameIdentifier, userDatatobject.Id.ToString()),
+                  new Claim(ClaimTypes.Name , userDatatobject.Username)
+              };
+
+            var key = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Key").Value));
+
+            var creds= new SigningCredentials (key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new    SecurityTokenDescriptor
+            {
+                Subject= new ClaimsIdentity(claims),
+                Expires= DateTime.Now.AddDays(1),
+                SigningCredentials=creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token=tokenHandler.CreateToken(tokenDescriptor);
+            {
+
+            };
+            return Ok(new {
+                token=tokenHandler.WriteToken(token)
+            });
+        }
+    }
+}
---
+++ DatingApp-API-Project/Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp_API_Project
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+           
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("AppSettings:Key").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+            }
+            
+                 
+        
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors("CorsPolicy");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseAuthentication();
+            app.UseMvc();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+        }
+    }
+}
